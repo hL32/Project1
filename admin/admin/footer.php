1ba718d11f570ae1c510b36fd0241ee89553ee37
hL32 <75103969+hL32@users.noreply.github.com>
2021-06-21 00:05:11 +0700

    Add files via upload

---
+++ admin/admin/footer.php
@@ -0,0 +1,18 @@
+<div class="clear-both"></div>
+    </div>
+    </div>
+    <div id="admin-footer">
+        <div class="container">
+            <div class="left-panel">
+                © Copyright hoanganh249
+            </div>
+            <div class="right-panel">
+                <a target="_blank" href="https://www.facebook.com/tolabomne/" title="Facebook admin "><img height="48" src="../images/facebook.png" /></a>
+                <a target="_blank" href="https://www.youtube.com/channel/UCd8aIzwdaoQ_Abp3frmeZSA" title="Youtube"><img height="48" src="../images/youtube.png" /></a>
+                <a target="_blank" href="https://www.instagram.com/consenyeuchomeo/" title="Youtube"><img height="48" src="../images/instagram.png" /></a>
+            </div>
+            <div class="clear-both"></div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ admin/admin/logout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Đăng xuất tài khoản</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            .box-content{
+                margin: 0 auto;
+                width: 800px;
+                border: 1px solid #ccc;
+                text-align: center;
+                padding: 20px;
+            }
+            #user_logout form{
+                width: 200px;
+                margin: 40px auto;
+            }
+            #user_logout form input{
+                margin: 5px 0;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- doan nay a viet php de logout nhe -->
+        <div id="user_logout" class="box-content">
+            <h1>Đăng xuất tài khoản thành công</h1>
+            <a href="">Đăng nhập lại</a>
+        </div>
+    </body>
+</html>
